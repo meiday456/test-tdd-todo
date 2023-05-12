47cdff997624c8be1d9eaeabee596871e21d26fc
meidya456 <sjkang0228@gmail.com>
2023-05-12 20:25:34 +0900

    useUserPlaceholder hook이 잘못 구현되어 수정 및 그에 따른 테스트 코드 수정 반영

--- cypress/e2e/test.cy.ts
+++ cypress/e2e/test.cy.ts
@@ -2,5 +2,16 @@ describe("TEST", () => {
   it("true is true", () => {
     expect(true).equal(true);
     expect(true).to.equal(true);
+    assert.equal(4, 4, "EQUAL!!!");
+    assert.equal(4, 4);
+    assert.equal(4, 5, "NOT EQUAL!!!");
+  });
+
+  context("assert test", () => {
+    it("isObject", () => {
+      const employee = {name: "Lakshay", age: 34};
+
+      assert.isObject(employee, "value is object");
+    });
   });
 });
